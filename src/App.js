5ae3a2349171e2c887da20d944bddfdf96393499
yoonpee <shinyoon5658@gmail.com>
2021-06-05 16:44:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react, {useEffect, useState} from 'react';
+import axios from 'axios';
 function App() {
+  const [weather, setWeather] = useState(null);
+  useEffect(()=>{
+     axios.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c91a324293a96409823db214a356d663')
+     .then((data)=>{
+       setWeather(data.data);
+       console.log(data.data);
+     })
+     .catch((err)=>{
+       console.log(err)
+     })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {weather && (
+       <div>
+         <h1>{weather.name}</h1>
+         <h2>{weather.main.humidity}</h2>
+         <h2>{weather.main.temp}%C</h2>
+         <h2>but it feels like {weather.main.temp}%C</h2>
+       </div>
+     )}
+      
     </div>
-  );
-}
+  )
 
+}
 export default App;
